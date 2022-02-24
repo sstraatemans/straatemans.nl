b629fdb3a8c4bb3a7d394b9c321e90798e7302a9
Steven Straatemans <steven@straatemans.nl>
2022-02-24 15:16:52 +0100

    loop through data

--- public/index.ts
+++ public/index.ts
@@ -1,29 +1,43 @@
 import data from './../src/data/index.json';
 
 const importedData: Data = data;
-const CHARACTER_DURATION = 5000;
+const CHARACTER_DURATION = 1000;
+let CHARACTER_COUNT = 0;
+let PARTS_COUNT = 0;
 
-const createRoll = (part: Element, label: string) => {
+const getCharacterCount = () => {
+  if (CHARACTER_COUNT + 1 >= importedData.length) {
+    CHARACTER_COUNT = 0;
+    return CHARACTER_COUNT;
+  }
+  return CHARACTER_COUNT++;
+};
+
+const createRoll = (part: Element, label: string, count: number = 0) => {
   let wrapper = document.createElement('div');
   wrapper.classList.add('wrapper');
 
   const currentText = document.createElement('div');
   currentText.innerText = part.innerHTML;
   const nextText = document.createElement('div');
-  nextText.innerText = label;
+  nextText.innerText = importedData[CHARACTER_COUNT][label];
 
   part.replaceChildren(wrapper);
   wrapper.appendChild(currentText);
   wrapper.appendChild(nextText);
 
+  if (count === PARTS_COUNT) {
+    getCharacterCount();
+  }
+
   wrapper.addEventListener('animationend', () => {
-    console.log('end transition');
-    part.innerHTML = label;
+    console.log('end tssssransition');
+    part.innerHTML = nextText.innerText;
     wrapper.classList.remove('new');
     wrapper = null;
 
     setTimeout(() => {
-      createRoll(part, importedData[1].labelStart);
+      createRoll(part, label, count);
     }, CHARACTER_DURATION);
   });
 
@@ -34,10 +48,11 @@ const createRoll = (part: Element, label: string) => {
 
 const initRoll = () => {
   const parts = document.querySelectorAll('.roll');
+  PARTS_COUNT = parts.length;
 
   setTimeout(() => {
-    createRoll(parts[0], importedData[0].labelStart);
-    createRoll(parts[1], importedData[0].labelEnd);
+    createRoll(parts[0], 'labelStart', 1);
+    createRoll(parts[1], 'labelEnd', 2);
   }, CHARACTER_DURATION);
 };
 
